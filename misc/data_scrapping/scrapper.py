c81fc1c5d7b155a6476a9e7e7dba4aa7f2295c2c
xcc <2867389537@qq.com>
2024-10-24 20:50:08 +0800

    extract sub urls

--- misc/data_scrapping/scrapper.py
+++ misc/data_scrapping/scrapper.py
@@ -11,6 +11,9 @@ from urllib.parse import urljoin
 import asyncio
 import math
 from typing import Callable, TypeAlias
+import re
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.common.by import By
 
 
 def get_driver():
@@ -29,13 +32,18 @@ def href_to_absolute(url: str, href: str):
     return urljoin(url, href)
 
 
+def _extract_links_from_source(source: str):
+    soup = BeautifulSoup(source, "html.parser")
+    links = set(a["href"] for a in soup.find_all("a", href=True))
+    links = [href_to_absolute(url, href) for href in links]
+    return links
+
+
 # Extract all the links in the website; return a set of links in absolute path.
 def extract_links(driver: WebDriver, url: str, wait_time: float = 2.0):
     driver.get(url)
     time.sleep(wait_time)
-    soup = BeautifulSoup(driver.page_source, "html.parser")
-    links = set(a["href"] for a in soup.find_all("a", href=True))
-    links = [href_to_absolute(url, href) for href in links]
+    links = _extract_links_from_source(driver.page_source)
     return links
 
 
@@ -120,6 +128,90 @@ def extract_links_recursively(
     )
 
 
+def _extract_switch_page_elements_from_source(source: str) -> list[dict[str, str]]:
+    numeric_elements = []
+    soup = BeautifulSoup(source, "html.parser")
+    for element in soup.find_all(string=True):
+        text = element.strip()
+        if re.fullmatch(r"\d+", text):
+            parent_tag = element.parent
+            numeric_elements.append(parent_tag)
+    return [{"tag": element.name, "text": element.text} for element in numeric_elements]
+
+
+def _extract_js_elements_from_links(links: list[str]) -> list[str]:
+    js_links = []
+    for link in links:
+        if "javascript" in link:
+            js_links.append(link)
+    return [{"tag": "a", "href": link} for link in js_links]
+
+
+async def _extract_target_url_from_dynamic_element_async(
+    element: dict[str, str],
+    base_url: str,
+    sema: asyncio.Semaphore,
+    base_wait_time: float = 2.0,
+    target_wait_time: float = 2.0,
+) -> str:
+    async with sema:
+        driver = get_driver()
+        driver.get(base_url)
+        await asyncio.sleep(base_wait_time)
+        tag_name = element["tag"]
+        if "href" in element:
+            href = element["href"]
+            selenium_element = driver.find_element(
+                By.XPATH, f"//{tag_name}[@href='{href}']"
+            )
+        else:
+            text = element["text"]
+            selenium_element = driver.find_element(
+                By.XPATH, f"//{tag_name}[text()='{text}']"
+            )
+        actions = ActionChains(driver)
+        actions.move_to_element(selenium_element).click().perform()
+        await asyncio.sleep(target_wait_time)
+        result = driver.current_url
+        close_driver(driver)
+        return result
+
+
+# Extract all the links in the website; return a set of links in absolute path.
+# Links are from <a href="...">, <a href="javascript:;"> and clickable page number elements.
+async def _extract_sub_urls_async(
+    url: str,
+    sema: asyncio.Semaphore,
+    base_wait_time: float = 2.0,
+    target_wait_time: float = 2.0,
+):
+    driver = get_driver()
+    driver.get(url)
+    await asyncio.sleep(base_wait_time)
+    source = driver.page_source
+    dynamic_elements = await asyncio.to_thread(
+        _extract_switch_page_elements_from_source, source
+    )
+
+    async def extract_links_from_dynamic_elements(elements):
+        tasks = []
+        for element in elements:
+            task = asyncio.create_task(
+                _extract_target_url_from_dynamic_element_async(
+                    element, url, sema, base_wait_time, target_wait_time
+                )
+            )
+            tasks.append(task)
+        return await asyncio.gather(*tasks)
+
+    dynamic_links = await extract_links_from_dynamic_elements(dynamic_elements)
+    static_links = await asyncio.to_thread(_extract_links_from_source, source)
+    js_elements = _extract_js_elements_from_links(static_links)
+    js_links = await extract_links_from_dynamic_elements(js_elements)
+    close_driver(driver)
+    return dynamic_links + js_links + static_links
+
+
 if __name__ == "__main__":
     # driver = get_driver()
     # url = "https://www.sjtu.edu.cn/"
@@ -128,9 +220,14 @@ if __name__ == "__main__":
     # print(links)
     # close_driver(driver)
 
-    start_url = "https://www.sjtu.edu.cn/"
-    result = extract_links_recursively(start_url, max_depth=2)
-    for url, links in result.items():
-        print(f"Links length in {url}:")
-        print(len(links))
-        print()
+    # start_url = "https://www.sjtu.edu.cn/"
+    # result = extract_links_recursively(start_url, max_depth=2)
+    # for url, links in result.items():
+    #     print(f"Links length in {url}:")
+    #     print(len(links))
+    #     print()
+
+    sema = asyncio.Semaphore(3)
+    url = "https://www.sjtu.edu.cn/tg/index.html"
+    result = asyncio.run(_extract_sub_urls_async(url, sema))
+    print(result)
